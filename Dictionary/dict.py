a2f70e9105ec989b89b1ac9c3dcd682c8e79623c
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2023-11-30 15:17:51 +0500

    Day_01 Dict

--- Dictionary/dict.py
+++ Dictionary/dict.py
@@ -7,83 +7,83 @@ There are build-in 4 Data Structures in Python
 
 
 Dict:
-    No duplicate, mutable and ordered collection.
-    key value pair
-    It's related to Hash Tables as in Hashing It contains the key:value pair.
+    - No duplicate
+    - mutable and ordered collection.
+    - key value pair
+    - It's related to HashTables in Hashing as it contains key:value pair.
+    - JSON format for data Request/Response.
+    - Its same like Object in Javascript.
+    - Widely Used Data Structure with Panadas
 
-Its like Object in Javascript and like a JSON format.
-Note: Very useful Data Structure as help in storing data, transferring data through API
+
+Note: Very useful Data Structure as help in storing data, transfering data through API
 
 
 """
 
 
 def dictionary():
-    # Creating the dict using Constructor
+    # === Using Constructor ====
     student_dict = dict(names='ABC', sections="B")
 
+    # ==== Normal Way ====
     section = {
         "Name": "Hassan",
         "Roll No": "Sp20-Bcs-114",
         "Section": "FA20-BCS-B",
         "Subject": "Artificial Intelligence"
-    }
-    print(section["Name"])
+    } ; print(section["Name"])
 
-    # Input using the student.
+    # ==== Input in Dict ==== 
     student = {}
-    for i in range(2):
-        name = input("Enter the name of student: ")
-        semester = input("Enter the Semester of Student")
+    for i in range(0, 2, 1):
+        name: str = input("Enter the name of student: ")
+        semester: int = int(input("Enter the Semester of Student"))
         student[name] = semester
     print(student)
 
-    # Nested Dictionary
+    # ==== Nested Dictionary  =====
     nested_dict = {1: ['Geeks', 'learn'], 2: 'For', 3: {'A': 'Welcome', 'B': 'To', 'C': 'Geeks'}}
     print(nested_dict)
     print(nested_dict[3]['B'])  # Guess ???
 
 
 def dict_func():
-    # Dict Update Function
-    student = dict([('Hassan', 7), ('Ali', 10)])
+    # ==== Dict Update Func ====
+    student = dict(Hassan = 7, Ali = 10)
     updated_list = {'Hamid': '6'}
     student.update(updated_list)
     print(student)  # Merge both dict.
 
-    # Using Get Method
+    # ==== Get Method ==== : Returns  Value
     print(student.get('Hamid'))
 
-    # Del method
+    #  ==== Del method ====
     del student['Ali']   # will del this key-value pair
 
 
 def advance_dict_func():
     dict1 = {1: "Python", 2: "Javascript", 3: "Mojo", 4: "Java"}
 
-    # copy()  # Now manipulation on dict2 not affect dict1
+    #  ==== copy() ===== : Manipulation on dict2 not affect dict1.
     dict2 = dict1.copy()
-    print(dict2)
 
-    # clear() :: erase all items (key-value pair)
-    dict1.clear()
-    print(dict1)
+    #  ==== clear() ==== : Erase all items (key-value pair)
+    dict1.clear() ; print(dict1)
 
-    # items() :: give all items. check the format of return
+    #  ==== items() ==== :: give all items. check return format.
     print(dict2.items())
 
-    # return keys()
+    #  ==== keys() ====
     print(dict2.keys())
 
-    # pop() the given key value
-    dict2.pop(4)
-    print(dict2)
+    # ==== pop() ====
+    dict2.pop(4); print(dict2)          # given key as args.
 
-    # popitem() pop from the last
-    dict2.popitem()
-    print(dict2)
+    #  ==== popitem() ====  :Pop from last.
+    dict2.popitem(); print(dict2)
 
-    # return all values()
+    # ==== values() =====
     print(dict2.values())
 
 
@@ -92,6 +92,7 @@ def dict_comprehension():
     values = [1, 2, 3, 4, 5]
 
     dict_declare = {k: v for (k, v) in zip(keys, values)}
+    print(dict_declare)
 
     dict_coding = {x.upper(): x * 3 for x in 'coding'}
     print(dict_coding)
@@ -100,4 +101,5 @@ def dict_comprehension():
 if __name__ == '__main__':
     # dictionary()
     # dict_func()
-    dict_comprehension()
+    advance_dict_func()
+    # dict_comprehension()
