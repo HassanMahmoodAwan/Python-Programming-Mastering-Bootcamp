823dd7f4f0f2422da09dcebae1d102777c591c38
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2023-12-16 00:11:45 +0500

    read file update

--- File Handling/ReadFile.py
+++ File Handling/ReadFile.py
@@ -8,10 +8,10 @@ Modes:
     r:  READ file.
     w:  WRITE in file.
     a:  APPEND in file.
-    r+: To read and write data into the file. The previous data in the file will be overridden.
-    
-    w+: To write and read data. It will override existing data.
-    a+: To append and read data from the file. It wont override existing data.
+    === Advance Modes ===
+        - r+: 
+        - w+: 
+        - a+: 
 
 
 Note: File handling in Python is slower than other programming languages, especially when dealing with large files. 
