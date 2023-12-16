1b4f2bc2a5f853692b37115533f3c36d7e1c5eae
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2023-12-17 00:00:02 +0500

    Multi Exception

--- Exceptional Handling/Exception.py
+++ Exceptional Handling/Exception.py
@@ -1,7 +1,31 @@
 """
 - Error in Python can be of two types:
-    1- ERROR
+    1- Syntax ERROR
     2- Exception
 
+Exception occur when Internal even stop flow of program.
 
 """
+
+# ====== Basic Exception ========
+try:
+    a = 10
+    b = '20'
+    c = a+b
+except TypeError:
+    print('Both need to be Int.')
+
+
+# ====== Multiple Exception =======
+try:
+    def func(a):
+        if a < 4:
+            b = a/(a-3)
+        print(b)
+
+    func(3)
+    func(5)
+except ZeroDivisionError:
+    print('Not divide by zero')
+except NameError:
+    print('variable not exist')
