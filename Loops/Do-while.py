e92fb81db8f90b0a7be4cc1cad940ab6c03f7571
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2023-12-28 22:22:05 +0500

    Do-while Loop

---
+++ Loops/Do-while.py
@@ -0,0 +1,19 @@
+# Do-while Loop excecute at least one time.abs
+
+counter: int = 1
+roomNo: str = '221000'
+while True:
+    user_pass: int = int(input(f'Enter the  {counter} try Password: '))
+
+    if user_pass == int(roomNo):
+        print('You Unlock It')
+        break
+
+    if user_pass != int(roomNo) and counter > 5:
+        print('Your Attempts Failed')
+        break
+    
+    counter += 1
+    continue
+
+
